524fbb06687c3a8f2f58cd05e9155f8303018813
bogdantro <sabertoothtri@gmail.com>
2022-11-27 19:20:54 +0200

    df

--- static/js/nav.js
+++ static/js/nav.js
@@ -6,15 +6,12 @@ window.addEventListener("scroll", function(){
 function menu(){
     const sidebar = document.getElementById('sidebar');
     const hamburger = document.getElementById('hamburger');
-    const navImg = document.getElementById('navImg');
 
     if (window.getComputedStyle(sidebar,null).getPropertyValue("opacity") == '0'){
         hamburger.classList.add('active');
         sidebar.classList.add('active');
-        navImg.classList.add('active');
     }else{
         hamburger.classList.remove('active');
         sidebar.classList.remove('active');
-        navImg.classList.remove('active');
     }
 }
