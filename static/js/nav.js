7d4278431d9fa4be94fc7522b1efdc928db84cba
bogdantro <sabertoothtri@gmail.com>
2022-11-29 10:36:19 +0200

    xc

--- static/js/nav.js
+++ static/js/nav.js
@@ -6,12 +6,19 @@ window.addEventListener("scroll", function(){
 function menu(){
     const sidebar = document.getElementById('sidebar');
     const hamburger = document.getElementById('hamburger');
+    const navbar = document.getElementById('mobileNav');
 
     if (window.getComputedStyle(sidebar,null).getPropertyValue("opacity") == '0'){
         hamburger.classList.add('active');
         sidebar.classList.add('active');
+        navbar.classList.add('menu');
+        document.body.style.overflow = "hidden";
+        document.documentElement.style.overflow = "hidden";
     }else{
         hamburger.classList.remove('active');
         sidebar.classList.remove('active');
+        navbar.classList.remove('menu');
+        document.body.style.overflow = "scroll";
+        document.documentElement.style.overflow = "scroll";
     }
 }
